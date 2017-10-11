71ac45a6b6af3424265ee8c84d71cf4f2f4c28a3
rahul-shiv <rahul.shivaprasad@gmail.com>
2017-10-11 22:48:11 +0530

    Update todofunc.c

--- todofunc.c
+++ todofunc.c
@@ -46,7 +46,7 @@ void insert(struct node** list)
   printf("Enter the following details! \n");
   char name[100];
   printf("Name of your task : ");
-  fgets(name,100,stdin);
+  scanf("%s",name);
   int priority;
   printf("\nPriority in 1-5 : ");
   scanf("%d",&priority);
