6095242e4030d31e48c1d316df74b59d15f10434
rahul-shiv <rahul.shivaprasad@gmail.com>
2017-10-11 19:35:09 +0530

    header file added

---
+++ todohead.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+struct node
+{
+    struct tm date;
+    int priority;
+    char task[100];
+    struct node *next;
+};
+
+
+void insert(struct node**);
+void task_completed(struct node**);
+void display(struct node*);
